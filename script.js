81b80844cd5bc352f7a9e86b34d957bdd9087f85
Soudiptya Kamley <soudiptyakamley@gmail.com>
2024-08-17 09:54:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+const apikey = "8c14e1b092b3f31056663f950298bdce";
+const apiUrl ="https://api.openweathermap.org/data/2.5/weather?units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon")
+
+async function checkWeather(city){
+	const response = await fetch(apiUrl + city +`&appid=${apikey}`);
+	var data=await response.json();
+    
+	// console.log(data);
+
+	if(response.status == 404)
+	{
+		document.querySelector(".error").style.display = "block";
+		document.querySelector(".weather").style.display = "none";
+	}
+	else
+	{
+	    document.querySelector(".city").innerHTML = data.name;
+	    document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°c";
+	    document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
+	    document.querySelector(".wind").innerHTML = data.wind.speed +" km/h";
+       
+
+        if(data.weather[0].main == "Clouds"){
+		   weatherIcon.src="clouds.png";
+	    }
+	    else if(data.weather[0].main == "Clear"){
+		   weatherIcon.src="clear.png";
+	    }
+	    else if(data.weather[0].main == "Rain"){
+		   weatherIcon.src="rain.png";
+	    }
+	    else if(data.weather[0].main == "Drizzle"){
+		  weatherIcon.src="drizzle.png";
+	    }  
+	    else if(data.weather[0].main == "Mist"){
+		  weatherIcon.src="mist.png";
+	    }
+
+	    document.querySelector(".weather").style.display = "block";
+	    document.querySelector(".error").style.display = "none";
+
+
+    }
+}
+// 	}
+
+// 	document.querySelector(".city").innerHTML = data.name;
+// 	document.querySelector(".temp").innerHTML = Math.round(data.main.temp)+"°c";
+// 	document.querySelector(".humidity").innerHTML = data.main.humidity+"%";
+// 	document.querySelector(".wind").innerHTML = data.wind.speed +" km/h";
+       
+
+//     if(data.weather[0].main == "Clouds"){
+// 		weatherIcon.src="clouds.png";
+// 	}
+// 	else if(data.weather[0].main == "Clear"){
+// 		weatherIcon.src="clear.png";
+// 	}
+// 	else if(data.weather[0].main == "Rain"){
+// 		weatherIcon.src="rain.png";
+// 	}
+// 	else if(data.weather[0].main == "Drizzle"){
+// 		weatherIcon.src="drizzle.png";
+// 	}
+// 	else if(data.weather[0].main == "Mist"){
+// 		weatherIcon.src="mist.png";
+// 	}
+
+// 	document.querySelector(".weather").style.display = "block";
+
+
+// }
+
+searchBtn.addEventListener("click",()=>{
+	checkWeather(searchBox.value);
+})
